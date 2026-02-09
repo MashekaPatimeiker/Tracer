d3cef7db86f16dfc234c01df0ae051d4f401dab8
MashekaPatimeiker <danya.mashevskiy.98@bk.ru>
2026-02-09 17:29:38 +0300

    gay commit

--- Tracer.Core.Tests/TracerTests.cs
+++ Tracer.Core.Tests/TracerTests.cs
@@ -1,38 +1,78 @@
 ﻿using System;
 using System.Threading;
+using System.Linq;
 using Xunit;
 
 namespace Tracer.Core.Tests
 {
     public class TracerTests
     {
-        [Fact]
-        public void SingleMethodTrace_ShouldRecordCorrectTime()
-        {
-            var tracer = new Tracer();
-            tracer.StartTrace();
-            Thread.Sleep(100);
-            tracer.StopTrace();
+        private readonly Tracer _tracer;
 
-            var result = tracer.GetTraceResult();
-            Assert.Single(result.Threads);
-            Assert.Single(result.Threads[0].Methods);
-            Assert.True(result.Threads[0].Methods[0].Time >= 100);
+        public TracerTests()
+        {
+            _tracer = new Tracer();
         }
 
         [Fact]
-        public void NestedMethods_ShouldHaveCorrectHierarchy()
+        public void DoesNotThrow()
         {
-            var tracer = new Tracer();
-            tracer.StartTrace();
-            tracer.StartTrace();
-            Thread.Sleep(50);
-            tracer.StopTrace();
-            tracer.StopTrace();
+            var exceptionCount = 0;
+            var threads = new Thread[10];
 
-            var result = tracer.GetTraceResult();
-            Assert.Single(result.Threads[0].Methods);
-            Assert.Single(result.Threads[0].Methods[0].Methods);
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(() =>
+                {
+                    try
+                    {
+                        _tracer.StartTrace();
+                        Thread.Sleep(5);
+                        _tracer.StopTrace();
+                        
+                        _tracer.StartTrace();
+                        Thread.Sleep(5);
+                        _tracer.StopTrace();
+                    }
+                    catch
+                    {
+                        Interlocked.Increment(ref exceptionCount);
+                    }
+                });
+                threads[i].Start();
+            }
+
+            foreach (var thread in threads) thread.Join();
+
+            Assert.Equal(0, exceptionCount);
+            
+            var result = _tracer.GetTraceResult();
+            Assert.All(result.Threads, thread => 
+            {
+                Assert.Equal(2, thread.Methods.Count);
+            });
+        }
+
+        private void WorkerMethod()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(50);
+            _tracer.StopTrace();
+        }
+
+        private void OuterMethod(string threadName)
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(20);
+            InnerMethod();
+            _tracer.StopTrace();
+        }
+
+        private void InnerMethod()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(15);
+            _tracer.StopTrace();
         }
     }
 }
